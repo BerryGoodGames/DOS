d15a0c52fe1ca901e8b1be45d72d6553c1127959
nmd9202@gmail.com <>
2022-11-12 01:40:45 +0100

    scripts from whm for multiplayer, textures

--- Assets/Scripts/Card/CCardNumber.cs
+++ Assets/Scripts/Card/CCardNumber.cs
@@ -6,4 +6,6 @@ public class CCardNumber : CCard
 {
     public int number;
     public override int AmountPerColor => number == 0 ? 1 : 2;
+
+
 }
---
+++ Assets/Scripts/UI/UIFollowEntity.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIFollowEntity : MonoBehaviour
+{
+    [HideInInspector] public GameObject entity;
+    [HideInInspector] public Vector2 offset;
+
+    private void LateUpdate()
+    {
+        Camera cam = Camera.main;
+
+        Transform target = entity.transform;
+
+        RectTransform rt = GetComponent<RectTransform>();
+        rt.position = cam.WorldToScreenPoint((Vector2)target.position + offset);
+
+        // dividing by scale of ball for some reason???? 
+        float scl = (float)(6.5 / cam.orthographicSize / 0.64);
+        rt.localScale = new(scl, scl);
+    }
+}
